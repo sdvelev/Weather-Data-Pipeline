4c1b5b6405c6f84395647050200df2574e4322a3
Stefan Velev <sdvelev@gmail.com>
2025-01-02 15:51:09 +0200

    refactor: reorganize script for Extract logic [daily weather analysis pipeline]

--- src/pipeline/daily_weather_analysis/extract_weather_historical_data.py
+++ src/pipeline/daily_weather_analysis/extract_weather_historical_data.py
@@ -1,12 +1,12 @@
-from datetime import datetime, timedelta
-from zoneinfo import ZoneInfo
-
+import httpx
 import psycopg2
-import pytz
 import os
+
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from prefect import get_run_logger
 from prefect import task
+from zoneinfo import ZoneInfo
 
 load_dotenv()
 
@@ -20,33 +20,29 @@ db_password = os.getenv("DB_PASSWORD")
 db_name = os.getenv("DB_NAME")
 db_host = os.getenv("DB_HOST")
 
-# @task(log_prints=True)
-# def task_generate_url(city: str):
-#     return f"{base_url}{path_url_realtime_api}?key={api_key}&q={city}"
-#
-# @task
-# def task_extract_current_weather_data(url: str):
-#     logger = get_run_logger()
-#     response = httpx.get(url)
-#     try:
-#         response.raise_for_status()
-#     except Exception as e:
-#         logger.exception(f"Could not retrieve current weather data with url: {url}")
-#         raise e
-#
-#     weather_data = response.json()
-#     return weather_data
 
-@task(log_prints=True)
+@task(retries=2, retry_delay_seconds=3, timeout_seconds=10, log_prints=True)
 def task_generate_historical_data_url(city: str, previous_date: str):
     return f"{base_url}{path_url_history_api}?key={api_key}&q={city}&dt={previous_date}"
 
-@task
+@task(retries=2, retry_delay_seconds=10, timeout_seconds=20, log_prints=True)
+def task_extract_weather_historical_data(url: str):
+    logger = get_run_logger()
+    response = httpx.get(url)
+    try:
+        response.raise_for_status()
+    except Exception as e:
+        logger.exception(f"Could not retrieve weather historical data with url: {url}")
+        raise e
+
+    return response.json()
+
+@task(retries=2, retry_delay_seconds=2, timeout_seconds=10, log_prints=True)
 def task_extract_astro_data(astro_data: dict):
     return {'sunrise': astro_data['sunrise'], 'sunset': astro_data['sunset'], 'moonrise': astro_data['moonrise'],
                   'moonset': astro_data['moonset'], 'moon_phase': astro_data['moon_phase']}
 
-@task
+@task(retries=2, retry_delay_seconds=10, timeout_seconds=60)
 def task_extract_city_id(location_data: dict):
     with psycopg2.connect(user=db_user, password=db_password,
                           host=db_host, dbname=db_name) as conn:
@@ -62,13 +58,13 @@ def task_extract_city_id(location_data: dict):
             result = cursor.fetchone()
             return result[0] if result else None
 
-@task
+@task(retries=2, retry_delay_seconds=2, timeout_seconds=10)
 def task_extract_previous_date(time_zone: str):
     date_obj = datetime.now(ZoneInfo(time_zone))
     previous_day = date_obj - timedelta(days=1)
     return previous_day.date()
 
-@task
+@task(retries=2, retry_delay_seconds=10, timeout_seconds=60)
 def task_extract_weather_record(city_id: str, previous_date: str):
     with psycopg2.connect(user=db_user, password=db_password,
                           host=db_host, dbname=db_name) as conn:
