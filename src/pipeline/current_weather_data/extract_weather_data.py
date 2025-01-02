c1b508897408ae0daabefb0f79a990aabc45d83a
Stefan Velev <sdvelev@gmail.com>
2025-01-02 15:48:41 +0200

    refactor: reorganize script for Extract logic [current weather pipeline]

--- src/pipeline/current_weather_data/extract_weather_data.py
+++ src/pipeline/current_weather_data/extract_weather_data.py
@@ -1,21 +1,24 @@
 import httpx
 import os
+
 from dotenv import load_dotenv
 from prefect import get_run_logger
 from prefect import task
 
 load_dotenv()
 
+api_key = os.getenv("WEATHER_API_KEY")
+
 base_url = "https://api.weatherapi.com"
 path_url_realtime_api = "/v1/current.json"
 path_url_history_api = "/v1/history.json"
-api_key = os.getenv("WEATHER_API_KEY")
 
-@task
+
+@task(retries=2, retry_delay_seconds=3, timeout_seconds=10, log_prints=True)
 def task_generate_url(city: str):
     return f"{base_url}{path_url_realtime_api}?key={api_key}&q={city}"
 
-@task
+@task(retries=2, retry_delay_seconds=10, timeout_seconds=20, log_prints=True)
 def task_extract_current_weather_data(url: str):
     logger = get_run_logger()
     response = httpx.get(url)
@@ -25,5 +28,4 @@ def task_extract_current_weather_data(url: str):
         logger.exception(f"Could not retrieve current weather data with url: {url}")
         raise e
 
-    weather_data = response.json()
-    return weather_data
+    return response.json()
