b388e5bb9aa2030e7dd1e75bf2679e91f9655175
Stefan Velev <sdvelev@gmail.com>
2025-01-02 15:48:57 +0200

    refactor: reorganize script for Transform logic [current weather pipeline]

--- src/pipeline/current_weather_data/transform_weather_data.py
+++ src/pipeline/current_weather_data/transform_weather_data.py
@@ -1,6 +1,7 @@
 from prefect import task
 
-@task
+
+@task(retries=2, retry_delay_seconds=2, timeout_seconds=6)
 def task_fill_direct_city_fields(weather_data: dict, city_data_to_insert: dict):
     city_data_to_insert['name'] = weather_data['location']['name']
     city_data_to_insert['region'] = weather_data['location']['region']
@@ -9,7 +10,7 @@ def task_fill_direct_city_fields(weather_data: dict, city_data_to_insert: dict):
     city_data_to_insert['latitude'] = weather_data['location']['lat']
     city_data_to_insert['longitude'] = weather_data['location']['lon']
 
-@task
+@task(retries=2, retry_delay_seconds=2, timeout_seconds=6)
 def task_fill_direct_weather_fields(weather_data: dict, weather_data_to_insert: dict):
     weather_data_to_insert['temp_c'] = weather_data['current']['temp_c']
     weather_data_to_insert['feels_like_c'] = weather_data['current']['feelslike_c']
@@ -23,21 +24,20 @@ def task_fill_direct_weather_fields(weather_data: dict, weather_data_to_insert: 
     weather_data_to_insert['cloud_perc'] = weather_data['current']['cloud']
     weather_data_to_insert['uv_index'] = weather_data['current']['uv']
 
-@task
+@task(retries=2, retry_delay_seconds=2, timeout_seconds=6)
 def task_transform_date_time_fields(weather_data: dict, weather_data_to_insert: dict):
     weather_data_to_insert['date'], weather_data_to_insert['time'] = weather_data['current']['last_updated'].split()
 
-@task
+@task(retries=2, retry_delay_seconds=2, timeout_seconds=6)
 def task_transform_wind_speed_mps(weather_data: dict, weather_data_to_insert: dict):
     weather_data_to_insert['wind_speed_mps'] = weather_data['current']['wind_kph'] / 3.6
 
-@task
+@task(retries=2, retry_delay_seconds=2, timeout_seconds=6)
 def degree_to_compass_dir(wind_degrees: int):
     dir_position = int((wind_degrees / 22.5) + 0.5)
     dirs_list = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]
     return dirs_list[(dir_position % 16)]
 
-@task
+@task(retries=2, retry_delay_seconds=2, timeout_seconds=6)
 def task_transform_wind_dir(weather_data: dict, weather_data_to_insert: dict):
     weather_data_to_insert['wind_dir'] = degree_to_compass_dir(weather_data['current']['wind_degree'])
-
