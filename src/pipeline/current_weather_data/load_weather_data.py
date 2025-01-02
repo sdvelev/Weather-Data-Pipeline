28839e3c55a8f9c7378f4c0c32a410cbc1389ee5
Stefan Velev <sdvelev@gmail.com>
2025-01-02 15:49:07 +0200

    refactor: reorganize script for Load logic [current weather pipeline]

--- src/pipeline/current_weather_data/load_weather_data.py
+++ src/pipeline/current_weather_data/load_weather_data.py
@@ -1,7 +1,6 @@
-from pathlib import Path
-
 import psycopg2
 import os
+
 from dotenv import load_dotenv
 from prefect import task
 from prefect.runtime import task_run
@@ -13,14 +12,14 @@ db_password = os.getenv("DB_PASSWORD")
 db_name = os.getenv("DB_NAME")
 db_host = os.getenv("DB_HOST")
 
+
 def generate_city_task_run_name():
     flow_name = task_run.task_name
     city_data_to_insert = task_run.parameters['city_data_to_insert']
     return (f"{flow_name}-for-{city_data_to_insert['name'].replace(' ', '-')}-"
             f"{city_data_to_insert['country'].replace(' ', '-')}")
 
-
-@task(task_run_name=generate_city_task_run_name, log_prints=True)
+@task(task_run_name=generate_city_task_run_name, retries=2, retry_delay_seconds=10, timeout_seconds=60, log_prints=True)
 def task_load_city_data_if_necessary(city_data_to_insert: dict):
     with psycopg2.connect(user=db_user, password=db_password,
                           host=db_host, dbname=db_name) as conn:
@@ -41,21 +40,12 @@ def task_load_city_data_if_necessary(city_data_to_insert: dict):
                 DO UPDATE SET name=EXCLUDED.name
                 RETURNING id
                 """, city_data_to_insert
-
-                # VALUES(%(name)s, %(region)s, %(country)s, %(time_zone)s,
-                # CAST(%(latitude)s AS FLOAT), CAST(%(longitude)s AS FLOAT))
-                # ON CONFLICT ON CONSTRAINT city_unique_constraint DO NOTHING
-                # RETURNING id
-
-                # {'name': location_data['name'], 'region': location_data['region'],
-                #  'country': location_data['country'], 'time_zone': location_data['tz_id'],
-                #  'latitude': location_data['lat'], 'longitude': location_data['lon']}
             )
             result_index = cursor.fetchone()[0]
             cursor.execute("SELECT setval('city_id_seq', (SELECT MAX(id) FROM city))")
             return result_index
 
-@task(log_prints=True)
+@task(retries=2, retry_delay_seconds=10, timeout_seconds=60, log_prints=True)
 def task_load_weather_data_if_necessary(weather_data_to_insert: dict, city_id: str):
     weather_data_to_insert['city_id'] = city_id
     with psycopg2.connect(user=db_user, password=db_password,
